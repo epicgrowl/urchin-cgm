6081f4cd7627e6f6b129dae964090d04789456d6
Mark Wilson <mark@warkmilson.com>
2016-08-18 23:29:45 -0700

    Include generated assets for config page

---
+++ config/js/generated/includes.js
@@ -0,0 +1,34 @@
+window.CONSTANTS = {"ELEMENTS": ["GRAPH_ELEMENT", "SIDEBAR_ELEMENT", "STATUS_BAR_ELEMENT", "TIME_AREA_ELEMENT", "BG_ROW_ELEMENT"], "CONFIG_URL": "https://mddub.github.io/urchin-cgm/config/", "GRAPH_EXTRA_BASAL_OFFSET": 1, "NIGHTSCOUT_DIRECTION_TO_TREND": {"Flat": 4, "NONE": 0, "SingleUp": 2, "DoubleDown": 7, "NOT_COMPUTABLE": 8, "SingleDown": 6, "RATE OUT OF RANGE": 9, "DoubleUp": 1, "FortyFiveDown": 5, "RATE_OUT_OF_RANGE": 9, "NOT COMPUTABLE": 8, "FortyFiveUp": 3}, "INTERVAL_SIZE_SECONDS": 300, "NO_DELTA_VALUE": 65536, "POINT_SHAPE": {"circle": 1, "rectangle": 0}, "REQUEST_TIMEOUT": 5000, "MSG_TYPE_PREFERENCES": 2, "DEXCOM_ERROR_CODE_MAX": 12, "BATTERY_LOC": {"none": 0, "statusRight": 1, "timeBottomRight": 5, "timeBottomLeft": 4, "timeTopRight": 3, "timeTopLeft": 2}, "POINT_STYLES": {"a": {"pointRightMargin": 0, "plotLine": false, "pointMargin": 0, "pointRectHeight": 3, "pointShape": "rectangle", "pointWidth": 3}, "c": {"pointMargin": 0, "pointShape": "circle", "pointWidth": 7, "pointRightMargin": 1, "plotLine": false}, "b": {"pointRightMargin": 0, "plotLine": false, "pointMargin": -1, "pointRectHeight": 3, "pointShape": "rectangle", "pointWidth": 3}, "d": {"pointRightMargin": 5, "plotLine": true, "pointMargin": 2, "plotLineWidth": 3, "pointShape": "circle", "pointWidth": 13}}, "DEVICE_STATUS_RECENCY_THRESHOLD_SECONDS": 1800, "GRAPH_EXTRA_BOLUS_BITS": 1, "MSG_TYPE_DATA": 1, "ALIGN": {"right": 2, "center": 1, "left": 0}, "LOCAL_STORAGE_KEY_CONFIG": "config", "VERSION": "0.0.10", "DEBUG": null, "DEXCOM_NOISE_STRINGS": {"1": "Cln", "3": "Med", "2": "Lgt", "4": "Hvy"}, "PROPERTIES": ["el", "enabled", "width", "height", "black", "bottom", "right"], "DEFAULT_CONFIG": {"pointMargin": 0, "basalGraph": false, "customLayout": {"elements": [{"el": 3, "right": false, "bottom": false, "enabled": true, "height": 20, "width": 100, "black": false}, {"el": 0, "right": false, "bottom": true, "enabled": true, "height": 0, "width": 100, "black": false}, {"el": 1, "right": false, "bottom": false, "enabled": false, "height": 0, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": true, "height": 20, "width": 100, "black": true}, {"el": 2, "right": false, "bottom": false, "enabled": true, "height": 32, "width": 100, "black": false}], "batteryLoc": "timeBottomLeft", "timeAlign": "right"}, "statusJsonUrl": "", "layout": "a", "plotLine": false, "statusText": "", "statusLine3": "none", "statusLine2": "none", "statusLine1": "none", "pointShape": "rectangle", "pointWidth": 3, "statusOpenAPSEvBG": false, "basalHeight": 15, "updateEveryMinute": false, "statusOpenAPSNetBasal": false, "statusRawCount": 2, "statusUrl": "", "topOfRange": 200, "nightscout_url": "", "bolusTicks": false, "mmol": false, "advancedLayout": false, "batteryAsNumber": false, "pointRightMargin": 0, "bottomOfRange": 70, "plotLineWidth": 1, "pointRectHeight": 3, "statusContent": "none", "bottomOfGraph": 40, "topOfGraph": 250, "hGridlines": 50}, "SCREEN_WIDTH": 144, "GRAPH_EXTRA_BASAL_BITS": 5, "GRAPH_EXTRA_BOLUS_OFFSET": 0, "MSG_TYPE_ERROR": 0, "LAYOUTS": {"a": {"elements": [{"el": 3, "right": false, "bottom": true, "enabled": true, "height": 0, "width": 100, "black": false}, {"el": 0, "right": true, "bottom": true, "enabled": true, "height": 52, "width": 75, "black": false}, {"el": 1, "right": false, "bottom": true, "enabled": true, "height": 52, "width": 25, "black": false}, {"el": 2, "right": false, "bottom": false, "enabled": true, "height": 12, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": false, "height": 0, "width": 100, "black": false}], "batteryLoc": "statusRight", "timeAlign": "center"}, "c": {"elements": [{"el": 0, "right": false, "bottom": false, "enabled": true, "height": 35, "width": 100, "black": false}, {"el": 1, "right": false, "bottom": false, "enabled": false, "height": 35, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": true, "height": 25, "width": 100, "black": true}, {"el": 2, "right": false, "bottom": false, "enabled": true, "height": 10, "width": 100, "black": false}, {"el": 3, "right": false, "bottom": false, "enabled": true, "height": 0, "width": 100, "black": false}], "batteryLoc": "statusRight", "timeAlign": "center"}, "b": {"elements": [{"el": 0, "right": true, "bottom": true, "enabled": true, "height": 52, "width": 75, "black": false}, {"el": 1, "right": false, "bottom": true, "enabled": true, "height": 52, "width": 25, "black": false}, {"el": 2, "right": false, "bottom": false, "enabled": true, "height": 10, "width": 100, "black": false}, {"el": 3, "right": false, "bottom": false, "enabled": true, "height": 0, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": false, "height": 0, "width": 100, "black": false}], "batteryLoc": "statusRight", "timeAlign": "center"}, "e": {"elements": [{"el": 0, "right": true, "bottom": true, "enabled": true, "height": 0, "width": 75, "black": false}, {"el": 1, "right": false, "bottom": true, "enabled": true, "height": 0, "width": 25, "black": false}, {"el": 2, "right": false, "bottom": false, "enabled": true, "height": 44, "width": 100, "black": false}, {"el": 3, "right": false, "bottom": false, "enabled": true, "height": 14, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": false, "height": 0, "width": 100, "black": false}], "batteryLoc": "timeBottomLeft", "timeAlign": "right"}, "d": {"elements": [{"el": 3, "right": false, "bottom": true, "enabled": true, "height": 23, "width": 100, "black": false}, {"el": 0, "right": false, "bottom": true, "enabled": true, "height": 0, "width": 100, "black": false}, {"el": 1, "right": false, "bottom": true, "enabled": false, "height": 0, "width": 100, "black": false}, {"el": 4, "right": false, "bottom": false, "enabled": true, "height": 25, "width": 100, "black": false}, {"el": 2, "right": false, "bottom": false, "enabled": false, "height": 0, "width": 100, "black": false}], "batteryLoc": "timeTopRight", "timeAlign": "left"}}, "IOB_RECENCY_THRESHOLD_SECONDS": 600};
+(function() { /* src/js/points.js */
+/* global module, window */
+
+function points(c) {
+  var p = {};
+
+  p.computeGraphWidth = function(layout) {
+    var graph = layout.elements.filter(function(e) { return c.ELEMENTS[e.el] === 'GRAPH_ELEMENT'; })[0];
+    if (graph !== undefined) {
+      return Math.round(c.SCREEN_WIDTH * graph.width / 100);
+    } else {
+      return 0;
+    }
+  };
+
+  p.computeVisiblePoints = function(width, config) {
+    var available = width - config.pointRightMargin;
+    var points = Math.floor((available + Math.max(0, config.pointMargin)) / (config.pointWidth + config.pointMargin));
+    return Math.max(0, points);
+  };
+
+  return p;
+}
+
+// TODO: include this in the config page in a better way
+if (typeof(module) !== 'undefined') {
+  module.exports = points;
+}
+if (typeof(window) !== 'undefined') {
+  window.points = points;
+}
+
+})();
